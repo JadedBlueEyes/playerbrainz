fc2ef420650cc57a3cda198a24cdf2535af447a6
Jade Ellis <jade@ellis.link>
2026-04-25 21:20:55 +0100

    chore: Webserver

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new().route("/", get(serve_index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn serve_index() -> &'static str {
+    r"Hey there! Looks like you've reached the API server.
+If you just want to play music, you probably shouldn't be here!
+"
 }
